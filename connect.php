dfd546e16818a8d77a85593f4b3fe4e7842eb0c2
Karabu <165090474+KarabuLounge@users.noreply.github.com>
2024-04-26 16:25:34 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -11,7 +11,7 @@
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into registration(fullName, email, number, groupsize, notes) values(?, ?, ?, ?, ?)");
+		$stmt = $conn->prepare("insert into karabu(fullName, email, number, groupsize, notes) values(?, ?, ?, ?, ?)");
 		$stmt->bind_param("ssiis", $fullName, $email, $number, $groupsize, $notes);
 		$execval = $stmt->execute();
 		echo $execval;
